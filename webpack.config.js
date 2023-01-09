c9822c5a2573114e9b9445ecd897d2eb7e109751
kks91 <kokyusik91@naver.com>
2023-01-09 23:53:33 +0900

    🔧 Add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+// ES6가 아닌 node의 module 문법
 module.exports = {
-  mode: 'development',
+  mode: 'none',
   // 최초의 진입점 key값에 파일 이름 설정 할 수 있음!
   entry: {
     kks: '/src/index.ts',
@@ -12,8 +13,16 @@ module.exports = {
     path: path.resolve(__dirname, './build'),
     clean: true,
   },
+  devServer: {
+    port: 9000,
+  },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -21,7 +30,11 @@ module.exports = {
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
