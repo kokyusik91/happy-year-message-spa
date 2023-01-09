6037aed45767365b525f756af510068ccf786199
kks91 <kokyusik91@naver.com>
2023-01-09 23:53:58 +0900

    ✨ implementing ts files for test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import './app'
+import './shared/api'
 
-console.log('index.ts 입니다!')
+class App {
+  constructor(element: HTMLElement) {
+    const h1Tag = document.createElement('h1')
+    h1Tag.innerText = '제목입니다!'
+    element.insertAdjacentElement('afterend', h1Tag)
+  }
+}
+
+new App(document.getElementById('root')! as HTMLElement)
