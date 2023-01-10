3eb49053b527b2b26a68ca7a404b790d8142d176
kks91 <kokyusik91@naver.com>
2023-01-10 10:44:43 +0900

    🔧 install scss-loader and set to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,11 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        exclude: /node_modules/,
+      },
     ],
   },
   plugins: [
