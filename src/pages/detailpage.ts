1f4b400da25a15fd392290d60f51e87fa23a2326
kks91 <kokyusik91@naver.com>
2023-01-13 00:14:37 +0900

    🚧 Implementing each of pages

--- src/pages/detailpage.ts
+++ src/pages/detailpage.ts
@@ -2,7 +2,7 @@ class DetailPage {
   constructor(private root: HTMLElement) {}
 
   makeTemplate() {
-    return `<h1>디테일 페이지 입니다!</h1>`
+    return `<h1>디테일 페이지 입니다! 🌁</h1>`
   }
 
   render() {
--- src/pages/writepage.ts
+++ src/pages/writepage.ts
@@ -4,14 +4,15 @@ class WritePage {
   constructor(private root: HTMLElement) {}
 
   makeTemplate() {
-    return `<h1>작성하기 페이지 입니다!</h1>`
+    return `<h1>작성하기 페이지 입니다! ✒️</h1>
+    <button class='htmlbtn2'>상세 페이지로 이동!</button>`
   }
   render() {
     this.root.innerHTML = this.makeTemplate()
 
-    const mainButton = document.querySelector('.toMain')
-    mainButton?.addEventListener('click', () => {
-      routerInstance.navigate('/')
+    const btn2 = document.querySelector('.htmlbtn2')
+    btn2?.addEventListener('click', () => {
+      routerInstance.navigate('/detail', { replace: true })
     })
   }
 }
