80f2ae07a4ab3f06be51abf2035cd3e9a44529f9
kks91 <kokyusik91@naver.com>
2023-01-12 20:15:00 +0900

    ♻️ Implement Router class and make instance for use navigate function

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,12 @@
-import './app'
-import './shared/api'
 import MainPage from './pages/mainpage'
 import DetailPage from './pages/detailpage'
 import WritePage from './pages/writepage'
 import notFoundPage from './pages/notFoundpage'
+import { Route } from './types/index'
 
 import '../styles/index.scss'
 
-type Route = {
-  path: string
-  component: any
-}
-
-class App {
-  private data: any
-  constructor(element?: HTMLElement) {}
-
-  // 화면에 템플릿을 그려주는 함수
-  static render(template: string) {
-    const body = document.querySelector('body')! as HTMLElement
-    body.innerHTML = template
-  }
-}
-
-class Router {
-  constructor() {
-    // pushState로 페이지 이동할때마다 해당하는 콘솔이 찍히게 한다.
-    const routes = [
-      {
-        path: '/',
-        component: () => console.log('메인페이지!'),
-      },
-      {
-        path: '/write',
-        component: () => console.log('작성하기 페이지!'),
-      },
-      {
-        path: '/edit',
-        component: () => console.log('메인페이지!'),
-      },
-    ]
-
-    let match = routes.find((route) => route.path === location.pathname)
-    console.log(match)
-    const button = document.querySelector('.button')
-    button?.addEventListener('click', () => {
-      let match = routes.find((route) => route.path === location.pathname)
-
-      Router.navigate('/write')
-      match?.component()
-    })
-
-    match?.component()
-  }
-
-  static navigate(url?: string) {
-    history.pushState('null', 'null', url)
-  }
-
-  static back() {
-    history.back()
-  }
-}
-
-const routes: Route[] = [
+const routes = [
   {
     path: '/',
     component: MainPage,
@@ -78,71 +21,42 @@ const routes: Route[] = [
   },
 ]
 
-const router = (routes: Route[]) => {
-  const match = routes.map((route) => {
-    return {
-      route,
-      isMatch: route.path === location.pathname,
+class Router {
+  constructor(private mainRoute: Route[], private root: HTMLElement) {
+    window.addEventListener('popstate', (e) => {
+      this.router()
+    })
+    window.addEventListener('DOMContentLoaded', () => {
+      this.router()
+    })
+  }
+
+  public navigate(url?: string) {
+    history.pushState('null', 'null', url)
+    this.router()
+  }
+
+  private router() {
+    const advancedRoutes = this.mainRoute?.map((route) => {
+      return {
+        route,
+        isMatch: route.path === window.location.pathname,
+      }
+    })
+
+    const targetPage = advancedRoutes.find((targetRoute) => targetRoute.isMatch)
+
+    if (targetPage) {
+      // 실제 타켓 페이지 인스턴스화
+      const componentInstance = new targetPage.route.component(this.root)
+      componentInstance.render()
+    } else {
+      this.root.innerHTML = new notFoundPage().render()
     }
-  })
-
-  const target = match.find((item) => item.isMatch === true)
-  console.log(target)
-
-  const root = document.querySelector('#root')! as HTMLElement
-  if (target) {
-    const obj = new target.route.component()
-    root.innerHTML = obj.render()
-    const toMain = document.querySelector('.toMain')
-    toMain?.addEventListener('click', () => {
-      history.pushState('null', 'null', '/')
-      router(routes)
-    })
-    const toWrite = document.querySelector('.toWrite')
-    const toDetail = document.querySelector('.toDetail')
-    toWrite?.addEventListener('click', () => {
-      history.pushState('null', 'null', '/write')
-      router(routes)
-    })
-    toDetail?.addEventListener('click', () => {
-      history.pushState('null', 'null', '/detail')
-      router(routes)
-    })
-  } else {
-    const obj = new notFoundPage()
-    root.innerHTML = obj.render()
-    const toMain = document.querySelector('.toMain')
-    toMain?.addEventListener('click', () => {
-      history.pushState('null', 'null', '/')
-      router(routes)
-    })
   }
 }
 
-// const toWrite = document.querySelector('.toWrite')
-// const toDetail = document.querySelector('.toDetail')
-// const toMain = document.querySelector('.toMain')
-
-// toWrite?.addEventListener('click', () => {
-//   history.pushState('null', 'null', '/write')
-//   router(routes)
-// })
-// toDetail?.addEventListener('click', () => {
-//   history.pushState('null', 'null', '/detail')
-//   router(routes)
-// })
-// toMain?.addEventListener('click', () => {
-//   history.pushState('null', 'null', '/')
-//   router(routes)
-// })
-
-window.addEventListener('popstate', (e) => {
-  console.log(e)
-  router(routes)
-})
-
-// new App(document.getElementById('root')! as HTMLElement)
-
-window.addEventListener('DOMContentLoaded', () => {
-  router(routes)
-})
+export const routerInstance = new Router(
+  routes,
+  document.getElementById('root')! as HTMLElement,
+)
