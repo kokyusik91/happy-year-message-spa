cc905fbc18a8343c64687a7fe6a9a950fb8d39ab
kks91 <kokyusik91@naver.com>
2023-01-12 20:12:46 +0900

    🚧 implementing each of pages

--- src/pages/writepage.ts
+++ src/pages/writepage.ts
@@ -1,6 +1,18 @@
+import { routerInstance } from '../index'
+
 class WritePage {
-  render() {
+  constructor(private root: HTMLElement) {}
+
+  makeTemplate() {
     return `<h1>작성하기 페이지 입니다!</h1>`
+  }
+  render() {
+    this.root.innerHTML = this.makeTemplate()
+
+    const mainButton = document.querySelector('.toMain')
+    mainButton?.addEventListener('click', () => {
+      routerInstance.navigate('/')
+    })
   }
 }
 
