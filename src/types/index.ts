f0471a6aabb9da4dc630923ecca11d02c3ba5d3d
kks91 <kokyusik91@naver.com>
2023-01-13 15:18:00 +0900

    🏷️ Add postService types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,30 @@
 export type Route = {
   path: string
-  component: any
+  page: any
 }
 
 export type Replace = {
   replace: boolean
+}
+
+export type postRequestModel = {
+  title: string
+  content: string
+  image: string
+}
+
+export type postsResponseModel = {
+  code: number
+  data: {
+    posts: Post[]
+  }
+}
+
+export type Post = {
+  postId: number
+  title: string
+  content: string
+  image: string
+  createdAt: string
+  updatedAt: string
 }
