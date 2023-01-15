4a8ee1fded5ebfdc9afdd6eb02ecb9e6920fb598
kks91 <kokyusik91@naver.com>
2023-01-15 15:34:25 +0900

    🐛 fix bug when refreshed in detail & editpage

--- webpack.config.js
+++ webpack.config.js
@@ -6,20 +6,21 @@ module.exports = {
   mode: 'none',
   // 최초의 진입점 key값에 파일 이름 설정 할 수 있음!
   entry: {
-    kks: '/src/index.ts',
+    main: './src/index.ts',
   },
   output: {
     filename: './js/[name].js',
     path: path.resolve(__dirname, './build'),
     clean: true,
+    publicPath: '/',
   },
   devServer: {
     port: 9000,
     hot: true,
     compress: true,
+
     // historyApiFallBack: 히스토리 API를 사용하는 SPA 개발시 설정한다. 404가 발생하면 index.html로 리다이렉트한다.
     historyApiFallback: true,
-    liveReload: true,
   },
   module: {
     rules: [
